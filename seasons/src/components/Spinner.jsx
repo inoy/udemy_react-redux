5195c3fdf86e0d30dba67c9f7e7a3877a3d85b83
inoy <7m7110123@gmail.com>
2021-04-19 16:30:08 +0900

    Specifying Default Props

--- seasons/src/components/Spinner.jsx
+++ seasons/src/components/Spinner.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Spinner = () => {
+const Spinner = ({ message }) => {
   return (
     <div className="ui active dimmer">
-      <div className="ui big text loader">Loading...</div>
+      <div className="ui big text loader">{message}</div>
     </div>
   );
+};
+
+Spinner.propTypes = {
+  message: PropTypes.string,
+};
+
+Spinner.defaultProps = {
+  message: "Loading...",
 };
 
 export default Spinner;
