9bf3ee504199be8843e4cc2396a6e4288f18fc00
inoy <7m7110123@gmail.com>
2021-04-19 17:53:39 +0900

    Creating Event Handlers

--- pics/src/components/SearchBar.jsx
+++ pics/src/components/SearchBar.jsx
@@ -3,13 +3,21 @@ import React from "react";
 class SearchBar extends React.Component {
   state = {};
 
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" placeholder="Search Text"></input>
+            <input
+              type="text"
+              placeholder="Search Text"
+              onChange={this.onInputChange}
+            ></input>
           </div>
         </form>
       </div>
