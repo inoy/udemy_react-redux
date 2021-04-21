98fed65b5c87e737d919fff9db8079eb5b920b80
inoy <7m7110123@gmail.com>
2021-04-21 15:09:18 +0900

    Rendering Lists

--- pics/src/components/ImageList.jsx
+++ pics/src/components/ImageList.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const ImageList = ({ images }) => {
   console.log(images);
   return <div>ImageList</div>;
+};
+
+ImageList.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default ImageList;
