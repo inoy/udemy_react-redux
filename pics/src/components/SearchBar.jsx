cc7f132d50e293f9c671ed1d57bc21018feafc55
inoy <7m7110123@gmail.com>
2021-04-21 09:33:29 +0900

    Solving Context Issues

--- pics/src/components/SearchBar.jsx
+++ pics/src/components/SearchBar.jsx
@@ -3,11 +3,10 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onFormSubmit(event) {
+  onFormSubmit = (event) => {
     event.preventDefault();
-    // TypeError: Cannot read property 'state' of undefined が発生する状態
     console.log(this.state.term);
-  }
+  };
 
   render() {
     return (
