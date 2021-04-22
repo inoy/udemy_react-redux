797b3f1a77c0edb0e5b0514bf2450a0c91d9eb23
inoy <7m7110123@gmail.com>
2021-04-22 11:14:21 +0900

    Building and Styling the Accordion

--- widget/src/components/Accordion.jsx
+++ widget/src/components/Accordion.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Accordion = ({ items }) => {
-  return <h1>{items.length}</h1>;
+  const renderedItems = items.map(({ title, content }) => {
+    return (
+      <React.Fragment key={title}>
+        <div className="title active">
+          <i className="dropdown icon"></i>
+          {title}
+        </div>
+        <div className="content active">
+          <p>{content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 Accordion.protoTypes = {
