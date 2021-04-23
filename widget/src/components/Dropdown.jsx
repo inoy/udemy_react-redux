b7c7f453177f794c3f4e70a3c90a65a38ab0fe6c
inoy <7m7110123@gmail.com>
2021-04-23 09:56:13 +0900

    A Lot of JSX

--- widget/src/components/Dropdown.jsx
+++ widget/src/components/Dropdown.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Dropdown = () => {
-  return <h1>Dropdown</h1>;
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map(({ value, label }) => {
+    return (
+      <div key={value} className="item">
+        {label}
+      </div>
+    );
+  });
+  console.log(renderedOptions);
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div className="ui selection dropdown visible active">
+          <i className="dropdown icon"></i>
+          <div className="text">Select Color</div>
+          <div className="menu visible transition">{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Dropdown.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default Dropdown;
