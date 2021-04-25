f89ef921bacc968ac508cd530adc0b89427b36be
inoy <7m7110123@gmail.com>
2021-04-25 11:35:49 +0900

    Making use of useRef

--- widget/src/components/Dropdown.jsx
+++ widget/src/components/Dropdown.jsx
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
 
   useEffect(() => {
     document.body.addEventListener(
       "click",
-      () => {
-        console.log("BODY CLICKED");
+      (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
         setOpen(false);
       },
       {
@@ -23,10 +26,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       <div
         key={option.value}
         className="item"
-        onClick={() => {
-          console.log("ITEM CLICKED");
-          onSelectedChange(option);
-        }}
+        onClick={() => onSelectedChange(option)}
       >
         {option.label}
       </div>
@@ -34,15 +34,12 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div className="ui form" ref={ref}>
       <div className="field">
         <label className="label">Select a Color</label>
         <div
           className={`ui selection dropdown ${open ? "visible active" : ""}`}
-          onClick={() => {
-            console.log("DROPDOWN CLICKED");
-            setOpen(!open);
-          }}
+          onClick={() => setOpen(!open)}
         >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
--- widget/src/index.js
+++ widget/src/index.js
@@ -2,4 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// eslint-disable-next-line no-undef
+if (module.hot) {
+  // eslint-disable-next-line no-undef
+  module.hot.accept();
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
