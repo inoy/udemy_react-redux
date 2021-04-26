5756cd11b59746410c171aed725d23edcbe7583d
inoy <7m7110123@gmail.com>
2021-04-26 16:12:13 +0900

    Implementing a Header for Navigation

--- widget/src/App.jsx
+++ widget/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Route from "./components/Route";
+import Header from "./components/Header";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
@@ -40,6 +41,7 @@ const App = () => {
 
   return (
     <>
+      <Header />
       <Route path="/">
         <Accordion items={items} />
       </Route>
