fa9fdcca7fc7f99862c7f5d0b5966a02f1cc026b
inoy <7m7110123@gmail.com>
2021-04-27 18:30:38 +0900

    Building Reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,15 @@
+export const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:30" },
+    { title: "All Start", duration: "3:15" },
+    { title: "I Want it That way", duration: "1:45" },
+  ];
+};
+
+export const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
