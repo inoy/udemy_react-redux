1cf3d73c76325faf38238b4df110ced443260ccb
inoy <7m7110123@gmail.com>
2021-04-27 09:19:39 +0900

    Using the Custom Hook

--- videoshooks/src/apis/youtube.js
+++ videoshooks/src/apis/youtube.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-const KEY = 'AIzaSyDVfeUxsJzp8Vs0xnFGlhC2zrllcqbC4zc';
+const KEY = process.env.REACT_APP_GOOGLE_YOUTUBE_DATA_API_KEY;
 
 export default axios.create({
-  baseURL: 'https://www.googleapis.com/youtube/v3',
+  baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
-    part: 'snippet',
+    part: "snippet",
     maxResults: 5,
     key: KEY,
   },
