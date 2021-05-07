fd18c0b54f181fa505cdb848d0a873da1be65e94
inoy <7m7110123@gmail.com>
2021-05-07 18:19:29 +0900

    Merging Lists of Records

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -12,6 +12,14 @@ const streamReducer = (state = {}, action) => {
     case FETCH_STREAM:
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
+    case FETCH_STREAMS:
+      return action.payload.reduce(
+        (newState, stream) => ({
+          ...newState,
+          [stream.id]: stream,
+        }),
+        state
+      );
     case DELETE_STREAM:
       return state.filter((s) => s !== action.payload.id);
     default:
