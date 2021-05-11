806b3626979980b4d30cf79adc74304288c8bee8
inoy <7m7110123@gmail.com>
2021-05-11 18:27:19 +0900

    Showing a Stream

--- streams/client/src/components/streams/StreamShow.jsx
+++ streams/client/src/components/streams/StreamShow.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamShow = () => {
-  return <>StreamShow</>;
-};
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-export default StreamShow;
+  render() {
+    if (!this.props.stream) {
+      return <>Loading...</>;
+    }
+    const { title, description } = this.props.stream;
+    return (
+      <>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state, onwProps) => ({
+  stream: state.streams[onwProps.match.params.id],
+});
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
